4f116dd07d9535d9804cbbc8c378c88c27f127fb
dohyeongkim97 <redsun731118@naver.com>
2025-07-29 21:58:24 +0900

    test3.py, api requestor developping

--- loaders/test3.py
+++ loaders/test3.py
@@ -31,4 +31,61 @@ headers = {
 }
 
 #%% 
-url = f'https://kr.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/{tier}/{division}?page={page}&api_key={my_api}'
+tier = 'PLATINUM'
+division = 'IV'
+queue = 'RANKED_SOLO_5x5'
+
+
+#%%
+def get_summoners_by_division(tier, division, queue='RANKED_SOLO_5x5', my_api=my_api):
+    url = f'https://kr.api.riotgames.com/lol/league-exp/v4/entries/{queue}/{tier}/{division}?page=1&api_key={my_api}'
+    res = requests.get(url, headers=headers)
+    time.sleep(0.85)
+    return res.json() if res.status_code == 200 else []
+
+
+# %%
+def get_matchlist(puuid, count=100, my_api=my_api):
+    url = f'https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count={count}&api_key={my_api}'
+    res = requests.get(url, headers=headers)
+    time.sleep(0.85)
+    return res.json() if res.status_code == 200 else []
+
+#%%
+def get_match_detail(match_id, my_api=my_api):
+    url = f'https://asia.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={my_api}'
+    res = requests.get(url, headers=headers)
+    time.sleep(0.85)
+    return res.json() if res.status_code == 200 else None
+
+summoners = get_summoners_by_division(tier, division)
+#%%
+
+# review the codes below after.
+for i in range(0, 20):
+    summ = summoners[i]
+    puuid = summ['puuid']
+    summoner_name = fn.get_summoner_name_by_puuid(puuid) or "Unknown"
+
+    match_ids = get_matchlist(puuid, count=5)
+    for match_id in match_ids:
+        match_detail = get_match_detail(match_id)
+        if match_detail:
+            match_summary = fn.extract_match_summary(match_detail, puuid)
+            if match_summary:
+                print({
+                    "tier": tier,
+                    "division": division,
+                    "summonerName": match_summary['player_name'],
+                    "puuid": puuid,
+                    "matchId": match_id,
+                    "win": match_summary['win'],
+                    "teamMembers": match_summary['team_members'],
+                    "enemyMembers": match_summary['enemy_members']
+                })
+
+# %%
+
+# 1. get_summoners_by_division에서 유저데이터 추출 후, puuid로 매치리스트 받아오기
+# 2. puuid로 매치리스트 받아온 후(get_matchlist), 각 매치의 상세정보 받아오기(get_match_detail)
+
